0b4bc5e308aed7bb1d2c5998ce2432c9a10ece3f
Koppány Ács <acs.koppany.copy@gmail.com>
2022-05-15 19:42:36 +0200

    Named placeholder not working

--- databasecontroller.cpp
+++ databasecontroller.cpp
@@ -3,6 +3,8 @@
 
 #include <QSqlDriver>
 #include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
 
 DatabaseController::DatabaseController()
 {
@@ -28,7 +30,7 @@ bool DatabaseController::createSchema(){
     connectToDatabase();
     if(db.isOpen()){
         QSqlQuery query;
-        query.exec("create table expenditures (id integer primary key autoincrement, name varchar(50), item_type varchar(50), amount_description varchar(50), value int, amount int)");
+        query.exec("create table expenditures (id integer primary key, name varchar(50), item_type varchar(50), amount_description varchar(50), value int, amount int)");
         return true;
     }
     else{
@@ -43,15 +45,32 @@ bool DatabaseController::addExpenditure(Expenditure exp)
     connectToDatabase();
     if(db.isOpen()){
         QSqlQuery query;
-        query.prepare("insert into expenditures (name, item_type,amount_description, value,amount)"
-                   ":name, :item_type, :amount_description, :value, :amount");
+        /*query.prepare("insert into expenditures (name, item_type, amount_description, value,amount)"
+                   "values(:name, :item_type, :amount_description, :value, :amount);");
         query.bindValue("name",exp.getName());
         query.bindValue("item_type",exp.getItemType());
         query.bindValue("amount_description",exp.getAmountDescription());
         query.bindValue("value",exp.getValue());
         query.bindValue("amount",exp.getAmount());
-        query.exec();
-        return true;
+        */
+        query.prepare("insert into expenditures (name, item_type, amount_description, value,amount)"
+                      "values (?, ?, ?, ?, ?)");
+        query.addBindValue(exp.getName());
+        query.addBindValue(exp.getItemType());
+        query.addBindValue(exp.getAmountDescription());
+        query.addBindValue(exp.getValue());
+        query.addBindValue(exp.getAmount());
+        //return query.exe();
+        if(query.exec()){
+            return true;
+        }
+        else{
+            //qDebug() << db.driver()->hasFeature(QSqlDriver::NamedPlaceholders);
+            qDebug() << query.lastError();
+            qDebug() << query.lastQuery();
+            return false;
+        }
+
     }
     else{
         qDebug("Failed to open database.");
